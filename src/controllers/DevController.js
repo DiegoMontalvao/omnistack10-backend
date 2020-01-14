17abdf9085daf83ac7c968a0dc133586050b66a6
Diego Montalvao <caals19@gmail.com>
2020-01-14 09:13:54 -0400

    criando a rota GET e inserindo o método index no DevController

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -3,30 +3,39 @@ const Dev = require ('../models/Dev');
 
 module.exports = {
 
+    async index(req, resp) {
+        const devs = await Dev.find();
+        return resp.json(devs);
+    },
+
     async store(req, resp) {
         const { github_username, techs, latitude, longitude } = req.body;
+
+        let dev = await Dev.findOne({ github_username });
+
+        if (!dev) {
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
     
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    
-        const { name = login, avatar_url, bio } = apiResponse.data;
-    
-        const techsArray = techs.split(',').map(tech => tech.trim());
-    
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude],
+            const { name = login, avatar_url, bio } = apiResponse.data;
+        
+            const techsArray = techs.split(',').map(tech => tech.trim());
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            };
+        
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            });
         };
-    
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location
-        });
-    
+
         return resp.json(dev);
-    
+
     }
 };
