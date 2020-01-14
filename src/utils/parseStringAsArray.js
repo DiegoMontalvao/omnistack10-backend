6e43150c334745287f2e0a7b328d9fa35515fc4a
Diego Montalvao <caals19@gmail.com>
2020-01-14 17:30:11 -0400

    corrigindo pequenos erros de portugues nos comentarios

--- src/utils/parseStringAsArray.js
+++ src/utils/parseStringAsArray.js
@@ -1,6 +1,6 @@
 // Tratando as tecnologias informadas pelo usuário no body que foram informadas como string, para converter em array
 //1º separando aonde houver virgula usando o método split
-//2º percorrendo o array usando o método map e eliminando os espações em branco usando o método trim retornando um array
+//2º percorrendo o array usando o método map e eliminando os espaços em branco usando o método trim retornando um array
 
 module.exports = function parseStringAsArry(arrayAsString){
     return arrayAsString.split(',').map(tech => tech.trim());
